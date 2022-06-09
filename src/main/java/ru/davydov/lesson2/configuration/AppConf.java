b5768c34912529e3520332e50659c4da3ac3eedc
samd82GB <samd82@yandex.ru>
2022-06-09 17:18:19 +0500

    Initial commit

---
+++ src/main/java/ru/davydov/lesson2/configuration/AppConf.java
@@ -0,0 +1,13 @@
+package ru.davydov.lesson2.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.davydov.lesson2")
+public class AppConf {
+
+
+
+
+}
